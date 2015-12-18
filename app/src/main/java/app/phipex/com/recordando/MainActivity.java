979ed40776d35a8ee45a0b11a494549220250d80
@phipex <phipex@yahoo.com>
2015-12-17 22:33:47 -0500

    feat:aplicada logica a la vista
    Primera parte del codigo que integra logica a la vista

--- app/src/main/java/app/phipex/com/recordando/MainActivity.java
+++ app/src/main/java/app/phipex/com/recordando/MainActivity.java
@@ -1,21 +1,58 @@
 package app.phipex.com.recordando;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import app.phipex.com.recordando.Util.TextChangeListener;
 
 
 public class MainActivity extends Activity {
+
+    private EditText txtNombre,txtTelefono, txtEmail,txtDireccion;
+    private Button btnAgregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        inicializarComponentesUI();
 
     }
 
+    private void inicializarComponentesUI() {
+        txtNombre = (EditText)findViewById(R.id.cmpNombre);
+        txtTelefono = (EditText)findViewById(R.id.cmpTelefono);
+        txtEmail = (EditText)findViewById(R.id.cmpEmail);
+        txtDireccion = (EditText)findViewById(R.id.cmpDireccion);
+        btnAgregar = (Button) findViewById(R.id.btnAgregar);
+        txtNombre.addTextChangedListener(new TextChangeListener() {
+            @Override
+            public void onTextChanged(CharSequence seq, int start, int before, int count) {
+                //super.onTextChanged(s, start, before, count);
 
+                btnAgregar.setEnabled(seq.toString().trim().isEmpty());//lo que hace es desactivar el boton cuando la secuencia es vacia
+
+            }
+        });
+    }
+
+
+    public void onClick(View view) {
+        String mesg = String.format("No ha sido agregado a la lista",txtNombre.getText());
+        Toast.makeText(this, mesg, Toast.LENGTH_SHORT).show();
+        btnAgregar.setEnabled(false);
+        limpiarCampos();
+    }
+
+    private void limpiarCampos() {
+        txtNombre.getText().clear();
+        txtTelefono.getText().clear();
+        txtEmail.getText().clear();
+        txtDireccion.getText().clear();
+        txtNombre.requestFocus();
+    }
 }
---
+++ app/src/main/java/app/phipex/com/recordando/Util/TextChangeListener.java
@@ -0,0 +1,24 @@
+package app.phipex.com.recordando.Util;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by sony vaio on 17/12/2015.
+ */
+public class TextChangeListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
